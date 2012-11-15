6bc9a59e0999617692565ab6d48bc2ef9c4250f9
root <root@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-11-15 15:28:53 +0000

    git-svn-id: svn://127.0.0.1/var/cvs/xu/FileCopy/trunk@165 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		File f = new File("E:\\MyDocuments\\My\\tm\\2.jpg");
+		File targetFile = new File(f.getAbsolutePath() + "_copy.jpg");
+			
+		InputStream is = new FileInputStream(f);
+		OutputStream os = new FileOutputStream(targetFile);
+		
+//		BufferedReader r = new BufferedReader(new InputStreamReader(is));
+//		PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter(targetFile)));
+		
+		byte[] b = new byte[1024];
+
+		while (is.available() > 0) {
+			is.read(b);
+			os.write(b);
+			
+		}
+		is.close();
+		os.close();
+		
+	}
+
+}
