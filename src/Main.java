c32088a6ea5e0166d9e5df03ede5923d5c0589e9
ux <ux@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-11-16 10:39:06 +0000

    git-svn-id: svn://127.0.0.1/var/cvs/xu/FileCopy/trunk@171 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -13,6 +15,78 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 
 
+/*class ExceptionA extends Exception {
+	
+}
+
+class ExceptionB extends Exception {
+	
+}
+
+class ExceptionBB extends ExceptionB {
+	
+}
+
+class ExceptionTest {
+	
+	int var = 0;
+	
+	public void test0() throws Exception {
+		try {			
+			if (var == 0) {
+				throw new ExceptionBB();
+			}
+			throw new ExceptionA();			
+		} catch(ExceptionA ex) {
+			System.out.println("A0");
+			throw ex;
+		} catch(Exception ex) { 
+			throw ex;
+		}
+		finally {
+			System.out.println("F0");
+		}
+	}
+	
+	public void test1() throws Exception {
+		try {
+			test0();
+		} finally {
+			System.out.println("F1");
+			throw new ExceptionB();
+		}
+	}
+	
+	public void test2() throws Exception {
+		try {
+			test1();			
+		} catch(ExceptionBB ex) {
+			System.out.println("EBB2");
+			throw ex;
+		} catch(ExceptionB ex) {
+			System.out.println("EB2");
+			throw new ExceptionA();
+		} catch(ExceptionA ex) {
+			System.out.println("EA2");
+			throw ex;
+		}
+	}
+	
+	public void test3() throws Exception {
+		try {
+			test2();
+		} catch(ExceptionA ex) {
+			System.out.println("EA3");
+		} catch(ExceptionB ex) {
+			System.out.println("EB3");
+		} finally {
+			System.out.println("F3");			
+		}
+	}
+	
+}
+*/
+
 public class Main {
 
 	/**
@@ -21,12 +95,26 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
+/*		try {
+			new ExceptionTest().test3();
+		} catch(Exception ex) {
+			System.out.println("EMAIN");
+			return;
+		}*/
+		
+		//long time = System.currentTimeMillis();
+		//myCopy("e:/cassiopeia-2012-08-23.z02", "e:/test.bin");
+		//System.out.println("time taken: " + (System.currentTimeMillis() - time) + " ms");
+		
+		//if (args.length == 0) {
+			//return;
+		//}
+		
 		File f = new File("E:\\MyDocuments\\My\\tm\\2.jpg");
 		File targetFile = new File(f.getAbsolutePath() + "_copy.jpg");
 			
 		InputStream is = null;
 		OutputStream os = null;
-		
 		try {
 			is = new FileInputStream(f);
 			os = new FileOutputStream(targetFile);
@@ -70,6 +158,35 @@ public class Main {
 		
 
 		
+	}
+	
+	public static void myCopy(String srcName, String dstName) throws IOException {
+		File src = new File(srcName);
+		File dst = new File(dstName);
+		
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = new FileInputStream(src);
+			os = new FileOutputStream(dst);
+			byte buf[] = new byte[1024*1024];
+			while(is.available() != 0) {				
+				os.write(buf, 0, is.read(buf));
+			}
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch(IOException e) {					
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch(IOException e) {					
+				}
+			}
+		}				
 	}
 
 }
